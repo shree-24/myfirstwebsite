5f5437085826e0c938a5c726d6b6baa9d1ac2be6
shrivallabh <shrivallabhmj@gmail.com>
2020-09-15 17:20:33 +0530

    cloud function

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,29 @@
 exports.handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "Welcome to the super secret area"
-  })
+
+  const secretContent = `
+       <h3>This is secret area</h3>
+       <p>Capacitor blocks DC and passes AC</p>
+       <p>Inductor blocks AC and passes DC</p>
+  `
+  let body
+
+  if (event.body) {
+    body = JSON.parse(event.body)
+  }else {
+    body = {}
+  }
+
+  if (body.password = "shree") {
+    callback(null, {
+      statusCode: 200,
+      body: secretContent
+    })
+
+  }else {
+    callback(null, {
+      statusCode: 401
+    })
+
+  }
+ 
 }
